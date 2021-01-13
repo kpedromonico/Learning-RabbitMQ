948563d8ecd48c5402299074c989d83b635eab33
Kaio Pedromonico <kpedromonico@gmail.com>
2021-01-13 15:38:50 +0100

    Adding RPC client & server

---
+++ RPCServer/RPCServer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RPCServer
+{
+    class RPCServer
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "rpc_queue", durable: false,
+                            exclusive: false, autoDelete: false, arguments: null);
+
+                    channel.BasicQos(0, 1, false);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    channel.BasicConsume(queue: "rpc_queue", autoAck: false, consumer: consumer);
+
+                    Console.WriteLine(" [x] Awaiting RPC requests");
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        string response = null;
+
+                        var body = ea.Body.ToArray();
+                        var props = ea.BasicProperties;
+                        var replyProps = channel.CreateBasicProperties();
+                        replyProps.CorrelationId = props.CorrelationId;
+
+                        try
+                        {
+                            var message = Encoding.UTF8.GetString(body);
+                            int n = int.Parse(message);
+                            Console.WriteLine(" [.] fib({0})", message);
+                            response = Fib(n).ToString();
+
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(" [.] " + e.Message);
+                            response = "";
+                        }
+                        finally
+                        {
+                            var responseBytes = Encoding.UTF8.GetBytes(response);
+                            channel.BasicPublish(exchange: "", routingKey: props.ReplyTo, basicProperties: replyProps, body: responseBytes);
+                            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                        }
+                    };
+
+                    Console.WriteLine("-- Press [enter] to exit --");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        public static int Fib(int i)
+        {
+            if (i == 0 || i == 1) return i;
+            return Fib(i - 1) + Fib(i - 2);
+        }
+    }
+}
